748374c0a9f5627e095693cde64b93d487a09115
Lucian <lucian.miller185@gmail.com>
2021-03-04 09:50:29 -0800

    It will update js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-const add = function(number1, number2) {
+function add(number1, number2) {
 	return number1 + number2;
-};
+}
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+const result = add(number1, number2);
+alert(result);
