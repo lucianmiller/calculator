fc7da977465325ca1122ea932aa5fb5c05310c5a
Lucian <lucian.miller185@gmail.com>
2021-03-04 09:56:38 -0800

    It will add a function that multiplies numbers

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,11 @@ function subtract(number1, number2) {
 	return number1 - number2;
 };
 
+function multiply(number1, number2) {
+	return number1 * number2;
+}
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = subtract(number1, number2);
+const result = multiply(number1, number2);
 alert(result);
