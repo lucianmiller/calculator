e868ab2290fe8bd09b2dca14174e435991a82b53
Lucian <lucian.miller185@gmail.com>
2021-03-04 09:54:31 -0800

    It will add a function that subtracts numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@ function add(number1, number2) {
 	return number1 + number2;
 }
 
+function subtract(number1, number2) {
+	return number1 - number2;
+};
+
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+const result = subtract(number1, number2);
 alert(result);
