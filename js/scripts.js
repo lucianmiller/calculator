cfc0ed3e54181ebc35293e258211d24c34ec7098
Lucian <lucian.miller185@gmail.com>
2021-03-23 11:20:17 -0700

    It will prevent pop up displaying NaN before user can submit anything

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,10 @@ function divide(number1, number2) {
 }
 
 $(document).ready(function() {
-	const number1 = parseInt($("#add1").val())
-	const number2 = parseInt($("#add2").val());
-	alert(add(number1, number2));
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#add1").val());
+		const number2 = parseInt($("#add2").val());
+		alert(add(number1, number2));
+	});
 });
