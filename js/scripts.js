3831be11a77a83af8243d3356987cfc7eea768c9
Lucian <lucian.miller185@gmail.com>
2021-03-23 11:14:33 -0700

    It will take in numbers from forms instead of promts

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,8 @@ function divide(number1, number2) {
 	return number1 / number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = divide(number1, number2);
-alert(result);
+$(document).ready(function() {
+	const number1 = parseInt($("#add1").val())
+	const number2 = parseInt($("#add2").val());
+	alert(add(number1, number2));
+});
